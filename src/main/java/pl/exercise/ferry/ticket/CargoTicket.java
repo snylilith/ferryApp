e5223a2eea1a84de0b0ebdf47e8e0e382feb3891
Lilianna Menazyk <liliannamenazyk@gmail.com>
2020-01-25 09:13:43 +0000

    Klasy biletów uzupełnione

--- src/main/java/pl/exercise/ferry/ticket/CargoTicket.java
+++ src/main/java/pl/exercise/ferry/ticket/CargoTicket.java
@@ -1,4 +1,13 @@
 package pl.exercise.ferry.ticket;
 
-public class CargoTicket {
+import java.math.BigDecimal;
+
+
+abstract class CargoTicket implements Ticket{
+
+
+    @Override
+    public BigDecimal getPrice() {
+        return BigDecimal.ZERO;
+    }
 }
--- src/main/java/pl/exercise/ferry/ticket/PersonTicket.java
+++ src/main/java/pl/exercise/ferry/ticket/PersonTicket.java
@@ -1,4 +1,19 @@
 package pl.exercise.ferry.ticket;
 
-public class PersonTicket {
+import pl.exercise.ferry.pax.PaxType;
+
+import java.math.BigDecimal;
+
+public abstract class PersonTicket implements Ticket{
+    private final PaxType paxType;
+
+
+    protected PersonTicket(PaxType paxType) {
+        this.paxType = paxType;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return BigDecimal.ZERO;
+    }
 }
